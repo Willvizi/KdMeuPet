0368537840d2a34f9b3032498a47a7f95905699d
Willian <williavizi@gmail.com>
2022-11-23 00:15:19 -0300

    Primeiro Commit

---
+++ app/src/main/java/com/williavizi/kdmeupetaplicacao/activity/LoginActivity.java
@@ -0,0 +1,117 @@
+package com.williavizi.kdmeupetaplicacao.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.williavizi.kdmeupetaplicacao.R;
+import com.williavizi.kdmeupetaplicacao.helper.ConfiguracaoFirebase;
+import com.williavizi.kdmeupetaplicacao.model.Usuario;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText campoEmail, campoSenha;
+    private Button botaoEntrar;
+    private ProgressBar progressBar;
+
+    private Usuario usuario;
+
+    private FirebaseAuth autenticacao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        verificarUsuarioLogado();
+        inicializarComponentes();
+
+        //Fazer login do usuario
+        progressBar.setVisibility( View.GONE );
+        botaoEntrar.setOnClickListener(v -> {
+
+            String textoEmail = campoEmail.getText().toString();
+            String textosenha = campoSenha.getText().toString();
+
+            if( !textoEmail.isEmpty() ){
+                if( !textosenha.isEmpty() ){
+
+                    usuario = new Usuario();
+                    usuario.setEmail( textoEmail );
+                    usuario.setSenha( textosenha );
+                    validarLogin( usuario );
+
+                }else{
+                    Toast.makeText(LoginActivity.this,
+                            "Preencha a senha!",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }else{
+                Toast.makeText(LoginActivity.this,
+                        "Preencha o e-mail!",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+    }
+
+    public void verificarUsuarioLogado(){
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        if( autenticacao.getCurrentUser() != null ){
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+    }
+
+    public void validarLogin( Usuario usuario ){
+
+        progressBar.setVisibility( View.VISIBLE );
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
+        autenticacao.signInWithEmailAndPassword(
+                usuario.getEmail(),
+                usuario.getSenha()
+        ).addOnCompleteListener(task -> {
+
+            if (!task.isSuccessful()) {
+                Toast.makeText(LoginActivity.this,
+                        "Erro ao fazer login",
+                        Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility( View.GONE );
+            } else {
+                progressBar.setVisibility( View.GONE );
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+
+        });
+
+
+    }
+
+    public void abrirCadastro(View view){
+        Intent i = new Intent(LoginActivity.this, CadastroActivity.class);
+        startActivity( i );
+    }
+
+    public void inicializarComponentes(){
+
+        campoEmail   = findViewById(R.id.editLoginEmail);
+        campoSenha   = findViewById(R.id.editLoginSenha);
+        botaoEntrar  = findViewById(R.id.buttonEntrar);
+        progressBar  = findViewById(R.id.progressLogin);
+
+        campoEmail.requestFocus();
+
+
+    }
+
+}
