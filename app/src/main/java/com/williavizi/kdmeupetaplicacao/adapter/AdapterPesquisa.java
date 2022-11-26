75b26f0a167d083a79dfa902da24198c4c020a4f
Willian <williavizi@gmail.com>
2022-11-26 17:08:12 -0300

    first commit

---
+++ app/src/main/java/com/williavizi/kdmeupetaplicacao/adapter/AdapterPesquisa.java
@@ -0,0 +1,4 @@
+package com.williavizi.kdmeupetaplicacao.adapter;
+
+public class adapterPesquisa {
+}
---
+++ app/src/main/java/com/williavizi/kdmeupetaplicacao/helper/UsuarioFirebase.java
@@ -0,0 +1,4 @@
+package com.williavizi.kdmeupetaplicacao.helper;
+
+public class UsuarioFirebase {
+}
